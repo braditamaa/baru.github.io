778f23e708c751db053fedbba8083e60c3b7f76e
Bagus Rizky Aditama <62259218+braditamaa@users.noreply.github.com>
2023-08-30 22:00:18 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 var links = [
-    "hhttps://wa.me/62895363076706",
-    "https://wa.me/62895331518808",
-    "https://goo.gl/maps/oBANjFGLUMpvY8js5"
+    "https://wa.me/6288216516238?text=Halo%20mas%20awan%20saya%20mau%20tanya%20barang%20elektronik",
+    "https://wa.me/6285727940701?text=Halo%20mas%20eko%20saya%20mau%20tanya%20barang%20elektronik",
+    "https://wa.me/62895421382934?text=Halo%20mas%20tony%20saya%20mau%20tanya%20barang%20elektronik",
+    "https://wa.me/6285642682431?text=Halo%20mas%20andre%20saya%20mau%20tanya%20barang%20elektronik",
+    "https://wa.me/6282220061614?text=Halo%20mas%20awan%20wiji%20mau%20tanya%20barang%20elektronik",
+    "https://wa.me/62895363076706?text=Halo%20mas%20bagus%20saya%20mau%20tanya%20barang%20elektronik",
+ 
 ];
 
 var randomIndex = Math.floor(Math.random() * links.length);
