59b9d131298b8e2f469e4723b5b8462babb1af5c
Bagus Rizky Aditama <62259218+braditamaa@users.noreply.github.com>
2023-08-31 07:03:04 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 var links = [
-    "https://wa.me/6288216516238?text=Halo%20mas%20awan%20saya%20mau%20tanya%20barang%20elektronik",
-    "https://wa.me/6285727940701?text=Halo%20mas%20eko%20saya%20mau%20tanya%20barang%20elektronik",
-    "https://wa.me/62895421382934?text=Halo%20mas%20tony%20saya%20mau%20tanya%20barang%20elektronik",
-    "https://wa.me/6285642682431?text=Halo%20mas%20andre%20saya%20mau%20tanya%20barang%20elektronik",
-    "https://wa.me/6282220061614?text=Halo%20mas%20awan%20wiji%20mau%20tanya%20barang%20elektronik",
-    "https://wa.me/62895363076706?text=Halo%20mas%20bagus%20saya%20mau%20tanya%20barang%20elektronik",
+    "https://wa.me/6288216516238",
+    "https://wa.me/6285727940701",
+    "https://wa.me/62895421382934",
+    "https://wa.me/6285642682431",
+    "https://wa.me/6282220061614",
+    "https://wa.me/62895363076706",
  
 ];
 
